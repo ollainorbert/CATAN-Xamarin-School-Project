2dd5653b92cf8714e86b7edad2db5a533238ed75
Norbert Ollai <ollainorbert10@gmail.com>
2017-10-25 03:28:07 +0200

    Making a special control.

--- NDEV.MasterClasses.Xamarin.Forms/MyXamarinSpecialControls.cs
+++ NDEV.MasterClasses.Xamarin.Forms/MyXamarinSpecialControls.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace NDEV.MasterClasses.Xamarin.Forms
 {
@@ -10,9 +11,73 @@ namespace NDEV.MasterClasses.Xamarin.Forms
     {
         public GridPlayerSettings() : base()
         {
-            /*this.RowDefinitions ={
-                new rowd
-            }*/
+            this._setTheGrid();
+            this._initThePlayerRowList();
+
         }
+
+        private List<GridPlayerRow> _gridPlayerRowList;
+
+        private const int MAX_PLAYER_NUMBER = 4;
+
+        private void _setTheGrid()
+        {
+            this.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            this.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            this.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            this.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+
+            this.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            this.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            this.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+        }
+
+        private void _initThePlayerRowList()
+        {
+            this._gridPlayerRowList = new List<GridPlayerRow>();
+            for(int i = 0; i < MAX_PLAYER_NUMBER; ++i)
+            {
+                this._gridPlayerRowList.Add(new GridPlayerRow());
+            }
+        }
+
+
+
+        public class GridPlayerRow
+        {
+            public GridPlayerRow()
+            {
+                this.Label = new MyXamarinLabel
+                {
+                    Text = string.Format(LABEL_TEXT_PATTERN, _playerNumber)
+                };
+
+                this.RadioButton = new MyXamarinTwoOptionRadioButton
+                {
+                    Button1Text = RADIO_BUTTON_PLAYER1_HUMAN,
+                    Button2Text = RADIO_BUTTON_PLAYER2_CPU
+                };
+
+                this.Entry = new MyXamarinEntry
+                {
+                    Placeholder = ENTRY_PLACEHOLDER
+                };
+
+                ++_playerNumber;
+            }
+
+            public MyXamarinLabel Label { get; private set; }
+            public MyXamarinTwoOptionRadioButton RadioButton { get; private set; }
+            public MyXamarinEntry Entry { get; private set; }
+
+            private static int _playerNumber = 1;
+
+            private const string LABEL_TEXT_PATTERN = "Player {0}";
+            private const string RADIO_BUTTON_PLAYER1_HUMAN = "Human";
+            private const string RADIO_BUTTON_PLAYER2_CPU = "CPU";
+            private const string ENTRY_PLACEHOLDER = "Enter name!";
+        }
+
+
     }
 }
